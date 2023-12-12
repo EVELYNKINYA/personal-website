dc1394d923eca925b1b853d8fdd5dcd4e1f61780
EVELYNKINYA <evelynkinya156@gmail.com>
2023-12-12 09:39:44 +0300

    add script.js-connected to index.html,style.csss

---
+++ js/script.js
@@ -0,0 +1,11 @@
+<div>
+    <p id="date"></p>
+
+  <script>
+	 var today = new Date();
+	 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+	 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	 var dateTime = date+' '+time;
+	 document.getElementById("date").innerHTML = dateTime;
+  </script>
+</div>
